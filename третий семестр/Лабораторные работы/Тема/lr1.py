e56f839594e695c0f626dccc571be5bdde5414f8
AlexTrubkina <55000120+AlexTrubkina@users.noreply.github.com>
2021-06-13 23:05:31 +0300

    Create lr1.py

---
+++ третий семестр/Лабораторные работы/Тема/lr1.py
@@ -0,0 +1,23 @@
+"""
+Программа, считающая площадь треугольника по формуле Герона:
+
+"""
+
+
+def main():
+    a = float(input("Введите а: "))
+    b = float(input("Введите b: "))
+    c = float(input("Введите с: "))
+    res = grn(a, b, c)
+    print(res)
+
+import math
+def grn(a, b, c):
+    """
+    Расчет площади треугольника по формуле Герона
+    """
+    ppr = (a+b+c) / 2
+    pl = pow(ppr * (ppr-a) * (ppr-b) * (ppr-c), 0.5)
+    return pl
+
+main()
